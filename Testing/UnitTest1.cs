02d8dc1c39676dfd76715de3fc723de70b2a92d2
up1 <somkiat.p@gmail.com>
2019-07-23 12:22:30 +0700

    Start project

---
+++ Testing/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using WorkshopNTL.Controllers;
+
+namespace Testing
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var p = new ValuesController();
+            Assert.True(p.check());
+        }
+    }
+}
